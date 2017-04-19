ceb1b67f507bf3cf1672266655ad8ae8cc9bb5d8
Sayak Brahmachari <admin@microcontrolled.nl>
2017-04-19 14:06:55 +0530

    HTML Indexing, Optimizations.
    Add HTML Indexing support, Reduce memory footprint while indexing.

--- indexer.py
+++ indexer.py
@@ -1,13 +1,13 @@
 import tools
+import pickle
 
 def index_files(files):
-    index = tools.HDict()
     for file in files:
+        print(file)
         file_content = open(file, 'rb').read()
         text = file_content.decode('ascii', 'ignore')
-        text = tools.process(text)
-        index[file] = text.split()
-    return index
+        text = tools.process(tools.filter_html(text))
+        with open('temp/{}.index'.format(tools.process(file)), 'wb') as dump: pickle.dump(index_keywords(text.split()), dump)
 
 def index_keywords(keywords):
     kwIndex = tools.HDict()
@@ -17,30 +17,27 @@ def index_keywords(keywords):
         else:
             kwIndex[word] = [index]
     return kwIndex
-
-def index_all_keywords(index):
-    total = tools.HDict()
-    for file in index.keys():
-        total[file] = index_keywords(index[file])
-    return total
-
-def invert_index(index):
+        
+def invert_index(files):
     invIndex = tools.HDict()
-    for file in index.keys():
-        for word in index[file].keys():
+    for file in files:
+        print(file)
+        with open('temp/{}.index'.format(tools.process(file)), 'rb') as dump: index = pickle.load(dump)
+        for word in index.keys():
             if word in invIndex.keys():
                 if file in invIndex[word].keys():
-                    invIndex[word][file].extend(index[file][word][:])
+                    invIndex[word][file].extend(index[word][:])
                 else:
-                    invIndex[word][file] = index[file][word]
+                    invIndex[word][file] = index[word]
             else:
-                invIndex[word] = {file: index[file][word]}
+                invIndex[word] = {file: index[word]}
     return invIndex
 
-def build_index(files=__import__('glob').glob('corpus/**/*.txt', recursive=True)):
-    import pickle
-    index = invert_index(index_all_keywords(index_files(files)))
-    with open('index.bin', 'wb') as file: pickle.dump(index, file)
+def build_index(files=__import__('glob').glob('corpus/**/*.html', recursive=True)):
+    input('Stage 1:\n')
+    index_files(files)
+    input('\n\nStage 2:\n')
+    with open('index.bin', 'wb') as file: pickle.dump(invert_index(files), file)
 
 if __name__ =='__main__':
     build_index()
--- tools.py
+++ tools.py
@@ -1,4 +1,4 @@
-import re
+import re, bs4
 from nltk.corpus import wordnet
 from textblob import Word
 
@@ -14,3 +14,14 @@ def process(string):
 class HDict(dict):
     def __hash__(self):
         return hash(frozenset(self.keys()))
+
+def filter_html(string):
+    soup = bs4.BeautifulSoup(string, 'html.parser')
+    elements = soup.strings
+    def visible(element):
+        if element.parent.name in ['meta', 'style', 'script', '[document]', 'head', 'title']:
+            return False
+        elif isinstance(element, bs4.element.Comment):
+            return False
+        return True
+    return ' '.join([element for element in elements if visible(element)])
