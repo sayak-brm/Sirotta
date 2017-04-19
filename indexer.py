14cf1120e2f4fe475c83a1f4bfb764cc8785294c
Sayak Brahmachari <admin@microcontrolled.nl>
2017-04-19 14:18:55 +0530

    Better temp-file handling.

--- indexer.py
+++ indexer.py
@@ -1,5 +1,5 @@
 import tools
-import pickle
+import pickle, tempfile, shutil
 
 def index_files(files):
     for file in files:
@@ -7,7 +7,7 @@ def index_files(files):
         file_content = open(file, 'rb').read()
         text = file_content.decode('ascii', 'ignore')
         text = tools.process(tools.filter_html(text))
-        with open('temp/{}.index'.format(tools.process(file)), 'wb') as dump: pickle.dump(index_keywords(text.split()), dump)
+        with open('{}/{}.index'.format(temp, tools.process(file)), 'wb') as dump: pickle.dump(index_keywords(text.split()), dump)
 
 def index_keywords(keywords):
     kwIndex = tools.HDict()
@@ -22,7 +22,7 @@ def invert_index(files):
     invIndex = tools.HDict()
     for file in files:
         print(file)
-        with open('temp/{}.index'.format(tools.process(file)), 'rb') as dump: index = pickle.load(dump)
+        with open('{}/{}.index'.format(temp, tools.process(file)), 'rb') as dump: index = pickle.load(dump)
         for word in index.keys():
             if word in invIndex.keys():
                 if file in invIndex[word].keys():
@@ -40,4 +40,7 @@ def build_index(files=__import__('glob').glob('corpus/**/*.html', recursive=True
     with open('index.bin', 'wb') as file: pickle.dump(invert_index(files), file)
 
 if __name__ =='__main__':
+    global temp
+    temp = tempfile.mkdtemp()
     build_index()
+    shutil.rmtree(temp)
